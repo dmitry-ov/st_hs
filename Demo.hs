9b0ca0eaea0d81f71b6f0092f7b521270e38643e
Dmitry Ovcharenko <d.ovcharenko@energocomm.ru>
2015-09-23 16:37:26 +0300

    implementation dist function

--- Demo.hs
+++ Demo.hs
@@ -5,10 +5,12 @@ import Data.Char
 test = isDigit '7'
 
 
---twoDigits2Int '4' '2'
-
 twoDigits2Int :: Char -> Char -> Int
-twoDigits2Int x y = if (isDigit x && isDigit y)
-                        then (digitToInt x)*10 + (digitToInt y)
-                            else 100
+twoDigits2Int x y = if isDigit x && isDigit y
+                    then (digitToInt x)*10 + (digitToInt y)
+                    else 100
+
+dist :: (Double, Double) -> (Double, Double) -> Double
+dist x y = sqrt((fst x - fst y)^2 + (snd x - snd y)^2)
+
 
