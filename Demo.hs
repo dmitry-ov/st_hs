b8d9e4f7ccf00be294a23881d13f773ba8a25e05
Dmitry Ovcharenko <d.ovcharenko@energocomm.ru>
2015-09-23 19:11:37 +0300

    implementation function factorial for negativate numbers

--- Demo.hs
+++ Demo.hs
@@ -14,3 +14,29 @@ dist :: (Double, Double) -> (Double, Double) -> Double
 dist x y = sqrt((fst x - fst y)^2 + (snd x - snd y)^2)
 
 
+--factorial :: Int -> Int
+--factorial 0 = 0
+--factorial n = factorial (n-1)
+
+doubleFact :: Integer -> Integer
+doubleFact 0 = 1
+doubleFact 1 = 1
+doubleFact n = n * doubleFact(n-2)
+
+
+
+factorial :: Integer -> Integer
+factorial n | n == 0    = 1
+            | n > 0     = n * factorial(n-1)
+            | otherwise = error "arg must be >=0"
+
+
+fibonacci :: Integer -> Integer
+fibonacci n | n > 0     = fibonacci(n-1) + fibonacci(n-2)
+            | n == 1    = 1
+            | n == 0    = 0
+            | n == (-1) = 1
+            | n == (-2) = (-1)
+            | n < 0  = if even n
+                       then (-1) * fibonacci(abs n)
+                       else fibonacci(abs n)
