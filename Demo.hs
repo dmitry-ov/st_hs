6161a729554b5e7ea4c1a14c4d2aa5a2a869a5ea
Dmitry Ovcharenko <d.ovcharenko@energocomm.ru>
2015-09-24 13:39:17 +0300

    add accumulate function help  for calculate fibonacci numbers

--- Demo.hs
+++ Demo.hs
@@ -31,12 +31,50 @@ factorial n | n == 0    = 1
             | otherwise = error "arg must be >=0"
 
 
+--factorial5 n | n > 0 = helper 1 n
+--             | otherwise = error "arg must be >= 0"
+
+--helper acc 0 = acc
+--helper acc n = helper (acc * n) (n-1)
+
+
 fibonacci :: Integer -> Integer
-fibonacci n | n > 0     = fibonacci(n-1) + fibonacci(n-2)
+fibonacci n | n > 0     =  help 0 1 n
             | n == 1    = 1
             | n == 0    = 0
             | n == (-1) = 1
             | n == (-2) = (-1)
             | n < 0  = if even n
-                       then (-1) * fibonacci(abs n)
-                       else fibonacci(abs n)
+                       then (-1) * help 0 1 (abs n)
+                       else help 0 1 (abs n)
+
+--fibonacci 10 = 55
+--fibonacci (-10) = (-55)
+
+help :: Integer -> Integer -> Integer -> Integer
+help acc1 acc2 n = if (n == 0) then acc1 else help acc2 (acc1 + acc2) (n-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
