f6fee9f1c24dfebf0c5f62add0039c5d7b825e94
Dmitry Ovcharenko <d.ovcharenko@energocomm.ru>
2015-10-26 13:41:27 +0300

    add avg function

--- Tree.hs
+++ Tree.hs
@@ -1,4 +1,10 @@
+--Тип бинарных деревьев можно описать следующим образом:
+
 data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
+
+--Реализуйте функцию height, возвращающую высоту дерева, и функцию size,
+--возвращающую количество узлов в дереве (и внутренних, и листьев).
+--Считается, что дерево, состоящее из одного листа, имеет высоту 0.
 
 height :: Tree a -> Int
 height (Leaf a) = 0
@@ -6,9 +12,20 @@ height (Node a b) = 1 + (max (height a) (height b))
 
 
 size :: Tree a -> Int
-size (Leaf a) = 0
-size (Node (Leaf a) (Leaf b)) = 1
+size (Leaf a) = 1
+size (Node (Leaf a) (Leaf b)) = 3
 size (Node a b) = 1 + size a + size b
 
--- let t = Node  (Node (Leaf 1) (Node (Leaf  3) (Leaf 7)))  (Leaf 4)
+
+
+avg :: Tree Int -> Int
+avg t =
+    let (c,s) = go t
+    in s `div` c
+  where
+    go :: Tree Int -> (Int,Int)
+    go = undefined
+
+
+--let t = Node  (Node (Leaf 1) (Node (Leaf  3) (Leaf 7)))  (Leaf 4)
 
